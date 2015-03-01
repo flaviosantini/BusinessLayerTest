c65d9ed6c7a2d199a26ee74905b4dfcf84e95f4d
flaviosantini <flaviosantini@me.com>
2015-03-01 08:18:46 -0500

    Creatin of Business Layer

---
+++ MASBusinessLayerTest/Data/DefaultMessageClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class DefaultMessageClass
+    {
+        public void Retrieve()
+        {
+            Console.WriteLine("Retrieving information from Data base...");
+        }
+    }
+}
